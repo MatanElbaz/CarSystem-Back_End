bf943d50c5d1334224e002c9b20726c6cf0be902
Yo <you@example.com>
2021-02-21 17:47:41 +0200

    .

--- CarSystemMatanElbaz/src/main/java/com/example/CarSystemMatanElbaz/model/CarType.java
+++ CarSystemMatanElbaz/src/main/java/com/example/CarSystemMatanElbaz/model/CarType.java
@@ -1,6 +1,9 @@
 package com.example.CarSystemMatanElbaz.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class CarType {
@@ -8,7 +11,6 @@ public class CarType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
 
     public CarType() {
 
